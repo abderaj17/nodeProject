183248f78a558c4fa49eb4d15e858fce95d12691
Adarsh Agrahari <agrahariadarsh68@gmail.com>
2025-02-13 21:16:48 +0530

    com

--- blog.js
+++ blog.js
@@ -1,2 +1,25 @@
 //blog management
 
+const express = require("express");
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log("Server is running on PORT 3000");
+});
+
+app.use();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Blog API");
+});
+
+app.post("/create", (req, res) => {
+  let blog = req.body;
+  blogs.push(blog);
+  res.send("Blog created successfully");
+});
+
+app.get("/blogs", (req, res) => {
+  res.json(blogs);
+});
